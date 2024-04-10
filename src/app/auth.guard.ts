6097961f5e566cd1eddd561dbcae250dff55b830
damien.jund <damien.jund@sogeti.com>
2024-04-10 14:20:29 +0200

    change guard

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,20 +1,15 @@
-import {CanActivate, Router} from '@angular/router';
-import {Injectable} from "@angular/core";
+import {Router} from '@angular/router';
+import {inject} from "@angular/core";
 import {AuthService} from "./auth.service";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
+export const AuthGuard = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
-  canActivate(): boolean {
-    if (this.authService.isLoggedIn) {
+  if (authService.isLoggedIn) {
       return true;
-    }
-
-    this.router.navigate(['/login']);
-    return false;
   }
+
+  router.navigate(['/login']);
+  return false;
 }
