3cb47038b7929a552167478a255f3accd3510e83
damien.jund <damien.jund@sogeti.com>
2024-04-10 13:58:41 +0200

    delete module

---
+++ src/app/pokemon/pokemon.routes.ts
@@ -0,0 +1,25 @@
+import {Routes} from "@angular/router";
+import {PokemonService} from "./pokemon.service";
+
+export default [{
+    path: '',
+    providers: [PokemonService],
+    children: [
+        {
+            path: 'edit/pokemon/:id',
+            loadComponent: () => import('./edit-pokemon/edit-pokemon.component').then(module => module.EditPokemonComponent)
+        },
+        {
+            path: 'add/pokemon',
+            loadComponent: () => import('./add-pokemon/add-pokemon.component').then(module => module.AddPokemonComponent)
+        },
+        {
+            path: 'pokemons',
+            loadComponent: () => import('./list-pokemon/list-pokemon.component').then(module => module.ListPokemonComponent)
+        },
+        {
+            path: 'pokemon/:id',
+            loadComponent: () => import('./detail-pokemon/detail-pokemon.component').then(module => module.DetailPokemonComponent)
+        },
+    ]
+}] as Routes;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,35 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {importProvidersFrom} from '@angular/core';
+import {AppComponent} from './app/app.component';
+import {AuthGuard} from './app/auth.guard';
+import {provideRouter, Routes} from '@angular/router';
+import {InMemoryDataService} from './app/in-memory-data.service';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        loadChildren: () => import('./app/pokemon/pokemon.routes')
+    },
+    {
+        path: 'login',
+        loadComponent: () => import("./app/login/login.component").then(module => module.LoginComponent)
+    },
+    {
+        path: '**',
+        loadComponent: () => import("./app/page-not-found/page-not-found.component").then(module => module.PageNotFoundComponent)
+    }
+];
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        importProvidersFrom(BrowserModule, FormsModule, HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })),
+        provideRouter(routes)
+    ]
+})
     .catch(err => console.error(err));
